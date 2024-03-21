8ca6f7185166143559cdc30360cf905544b8a2ba
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-03-21 08:59:03 +0530

    Add : The require dependecy added.

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 const express = require("express");
+const colors = require("colors");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+// Configure the dotenv
+dotenv.config();
 
 // Rest Object
 const app = express();
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
 // route
 app.get("/", (req, res) => {
-  res.status(200).send(`<h1>Welcome to Food App</h1>`);
+  res.status(200).send(`<h1>Welcome to Food App Server</h1>`);
 });
 
 // PORT
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 //Listen
 app.listen(PORT, () => {
-  console.log(`The server is running on port : ${PORT}`);
+  console.log(`The server is running on port : ${PORT}`.bgGreen.white);
 });
