1175c9b42b7cf7f59d90f3703493bc64e723a71c
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-03-21 09:48:09 +0530

    Add : user model added successfully.

---
+++ models/userModel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    address: {
+      type: Array,
+    },
+    phone: {
+      type: String,
+      required: [true, "phone number is required"],
+    },
+    userType: {
+      type: String,
+      required: [true, "user type is required"],
+      default: "client",
+      enum: ["client", "admin", "vendor", "driver"],
+    },
+    profile: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_640.png",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
