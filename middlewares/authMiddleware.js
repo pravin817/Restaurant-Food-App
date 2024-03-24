2b1463a8e0c64fcd00a388866734f0e0aafdc39e
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-03-24 09:21:33 +0530

    Fix : authmiddleware  fixed.

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -7,22 +7,22 @@ const authMiddleware = async (req, res, next) => {
     // console.table([token, typeof token]);
     const tokenValue = token.split(" ")[1];
 
-    const decoded = jwt.verify(tokenValue, process.env.JWT_SECRET);
-    console.log("Decoded : ", decoded);
-
-    if (decoded.id) {
-      req.body.id = decoded.id;
-      next();
-    } else {
-      return res.status(401).send({
-        message: "Unauthorized Access",
-        status: false,
-      });
-    }
+    jwt.verify(tokenValue, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        res.status(401).send({
+          message: "Un-authorised user",
+          status: false,
+          err,
+        });
+      } else {
+        req.body.id = decoded._id;
+        next();
+      }
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send({
-      message: "Error while authenticating the user",
+      message: "Please provide auth token",
       status: false,
       error,
     });
