0823054f96a75cdda115b2517f8a4841b9b963d8
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-03-31 08:13:25 +0530

    Feature : API food related added.
    1) Get all food
    2) Get single food by Id
    3) Get food list by restaurant

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -56,6 +56,108 @@ const createFoodController = async (req, res) => {
   }
 };
 
+// Get All food || GET
+const getAllFoodController = async (req, res) => {
+  try {
+    const allFoods = await Food.find();
+
+    if (allFoods.length === 0) {
+      return res.status(404).send({
+        message: "No food found",
+        success: false,
+      });
+    }
+
+    res.status(200).send({
+      message: "All foods feteched successfully",
+      success: true,
+      foods: allFoods,
+      totalCount: allFoods.length,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "Error while fetching the all foods",
+      success: false,
+      error,
+    });
+  }
+};
+
+// Get single Food || GET
+const getSingleFoodController = async (req, res) => {
+  try {
+    const foodId = req.params.id;
+
+    if (!foodId) {
+      return res.status(400).send({
+        message: "Please provide the food ID",
+        success: false,
+      });
+    }
+
+    const food = await Food.findById({ _id: foodId });
+
+    if (!food) {
+      return res.status(404).send({
+        message: "No food found",
+        success: false,
+      });
+    }
+
+    res.status(200).send({
+      message: "Food fetched successfully",
+      success: true,
+      food,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "Error while fetching the single food",
+      success: false,
+      error,
+    });
+  }
+};
+
+// Get Food by the restaurant || GET
+const getFoodsByRestaurantController = async (req, res) => {
+  try {
+    const restaurantId = req.params.id;
+
+    if (!restaurantId) {
+      return res.status(400).send({
+        message: "Please provide the restaurant ID",
+        success: false,
+      });
+    }
+
+    const food = await Food.find({ restaurant: restaurantId });
+    if (!food) {
+      return res.status(404).send({
+        message: "No food found for this restaurant",
+        success: false,
+      });
+    }
+
+    res.status(200).send({
+      message: "Foods fetched successfully",
+      success: true,
+      food,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "Error while fetching the foods by restaurant",
+      success: false,
+      error,
+    });
+  }
+};
+
 module.exports = {
   createFoodController,
+  getAllFoodController,
+  getSingleFoodController,
+  getFoodsByRestaurantController,
 };
--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
-const { createFoodController } = require("../controllers/foodController");
+const {
+  createFoodController,
+  getAllFoodController,
+  getSingleFoodController,
+  getFoodsByRestaurantController,
+} = require("../controllers/foodController");
 
 const router = express.Router();
 
 // Create the Food || POST
 router.post("/create", authMiddleware, createFoodController);
+
+// Get all the foods || GET
+router.get("/getAll", getAllFoodController);
+
+// Get a single food || GET
+router.get("/get/:id", getSingleFoodController);
+
+// Get Food by the restaurant || GET
+router.get("/getByRestaurant/:id", getFoodsByRestaurantController);
 
 module.exports = router;
